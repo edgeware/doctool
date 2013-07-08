a4e8fded08dc3294e70daef6980d5ccd2ce54be7
Jesper Brown <jesper.brown@gmail.com>
2013-07-08 15:47:32 +0200

    Bumped version to 0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='doctool',
-    version=0.5,
+    version=0.6,
     packages=['doctool'],
     package_data={'doctool': ['style/*']},
     entry_points={
